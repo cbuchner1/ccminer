c218c3f514b3be38c227b630b63dc58f8105ffd7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-11-28 22:18:45 +0100

    quark/anime: +100KH, bmw tpb was not correct
    This small change also enhance a bit x11..17 algos

--- quark/cuda_bmw512.cu
+++ quark/cuda_bmw512.cu
@@ -286,7 +286,7 @@ __host__ void quark_bmw512_cpu_setBlock_80(void *pdata)
 
 __host__ void quark_bmw512_cpu_hash_64(int thr_id, int threads, uint32_t startNounce, uint32_t *d_nonceVector, uint32_t *d_hash, int order)
 {
-    const int threadsperblock = 256;
+    const int threadsperblock = 128;
 
     // berechne wie viele Thread Blocks wir brauchen
     dim3 grid((threads + threadsperblock-1)/threadsperblock);
@@ -301,7 +301,7 @@ __host__ void quark_bmw512_cpu_hash_64(int thr_id, int threads, uint32_t startNo
 
 __host__ void quark_bmw512_cpu_hash_80(int thr_id, int threads, uint32_t startNounce, uint32_t *d_hash, int order)
 {
-    const int threadsperblock = 256;
+    const int threadsperblock = 128;
 
     // berechne wie viele Thread Blocks wir brauchen
     dim3 grid((threads + threadsperblock-1)/threadsperblock);
