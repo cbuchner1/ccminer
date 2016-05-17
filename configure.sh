b1db3a230beb101a2b8d60a8b4bf48d11cf5ceb2
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-05-17 20:27:28 +0200

    linux: allow to compile on ubuntu 16.04

--- configure.sh
+++ configure.sh
@@ -8,5 +8,6 @@
 
 extracflags="-march=native -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
-CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall" ./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
+CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall  -D_FORCE_INLINES" \
+	./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
 
