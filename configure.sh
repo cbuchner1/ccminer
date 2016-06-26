05552013843a3738bd4a70d7ad270d592d955533
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-06-26 20:31:31 +0200

    linux: return to default cuda, mistake in a nvapi commit

--- configure.sh
+++ configure.sh
@@ -1,13 +1,7 @@
-# possible additional CUDA_CFLAGS
-#-gencode=arch=compute_52,code=\"sm_52,compute_52\"
-#-gencode=arch=compute_50,code=\"sm_50,compute_50\"
-#-gencode=arch=compute_35,code=\"sm_35,compute_35\"
-#-gencode=arch=compute_30,code=\"sm_30,compute_30\"
-
-#--ptxas-options=\"-v -dlcm=cg\""
+# To change the cuda arch, edit Makefile.am and run ./build.sh
 
 extracflags="-march=native -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
 CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall  -D_FORCE_INLINES" \
-	./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda-6.5 --with-nvml=libnvidia-ml.so
+	./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
 
