ebef3c865680c273ca396c7ed60df566f68f430b
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2017-03-07 18:40:31 +0100

    sha256t algo (3x sha256, not sha256d)
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>

--- bench.cpp
+++ bench.cpp
@@ -71,6 +71,7 @@ void algo_free_all(int thr_id)
 	free_qubit(thr_id);
 	free_skeincoin(thr_id);
 	free_skein2(thr_id);
+	free_sha256t(thr_id);
 	free_sia(thr_id);
 	free_sib(thr_id);
 	free_s3(thr_id);
--- ccminer.cpp
+++ ccminer.cpp
@@ -251,6 +251,7 @@ Options:\n\
 			penta       Pentablake hash (5x Blake 512)\n\
 			quark       Quark\n\
 			qubit       Qubit\n\
+			sha256t     SHA256 x3\n\
 			sia         SIA (Blake2B)\n\
 			sib         Sibcoin (X11+Streebog)\n\
 			scrypt      Scrypt\n\
@@ -878,6 +879,7 @@ static bool submit_upstream_work(CURL *curl, struct work *work)
 		case ALGO_BLAKECOIN:
 		case ALGO_BLAKE2S:
 		case ALGO_BMW:
+		case ALGO_SHA256T:
 		case ALGO_VANILLA:
 			// fast algos require that... (todo: regen hash)
 			check_dups = true;
@@ -2099,6 +2101,7 @@ static void *miner_thread(void *userdata)
 			case ALGO_BLAKE:
 			case ALGO_BMW:
 			case ALGO_DECRED:
+			case ALGO_SHA256T:
 			//case ALGO_WHIRLPOOLX:
 				minmax = 0x40000000U;
 				break;
@@ -2291,6 +2294,9 @@ static void *miner_thread(void *userdata)
 			break;
 		case ALGO_SKEIN2:
 			rc = scanhash_skein2(thr_id, &work, max_nonce, &hashes_done);
+			break;
+		case ALGO_SHA256T:
+			rc = scanhash_sha256t(thr_id, &work, max_nonce, &hashes_done);
 			break;
 		case ALGO_SIA:
 			rc = scanhash_sia(thr_id, &work, max_nonce, &hashes_done);
